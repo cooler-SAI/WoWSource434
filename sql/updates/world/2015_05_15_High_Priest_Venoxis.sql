4d069463e3473f5f35b8fe4df45a42c341a4be0a
cooler-SAI <cooler19860212@yahoo.com>
2016-10-23 21:57:00 +0300

    Added Databases

--- sql/updates/world/2015_05_15_High_Priest_Venoxis.sql
+++ sql/updates/world/2015_05_15_High_Priest_Venoxis.sql
@@ -2,11 +2,11 @@
 UPDATE `creature_template` SET `lootid` = 52155 WHERE `entry` = 52155;
  
 DELETE FROM `creature_loot_template` WHERE `Entry` = 52155;
-INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `LootMode`, `MinCount`, `MaxCount`) VALUES
-(52155, 521550, 521550, 100, 1, 1, 1);
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(52155, 521550, 100, 521550, 1, 1, 1);
  
 DELETE FROM `reference_loot_template` WHERE `Entry` = 521550;
-INSERT INTO `reference_loot_template` (`Entry`, `Item`, `Chance`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`) VALUES
+INSERT INTO `reference_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
 (521550, 69600, 0, 1, 1, 1, 1), -- Belt of Slithering Serpents
 (521550, 69603, 0, 1, 1, 1, 1), -- Breastplate of Serenity
 (521550, 69604, 0, 1, 1, 1, 1), -- Coils of Hate
@@ -17,11 +17,11 @@ INSERT INTO `reference_loot_template` (`Entry`, `Item`, `Chance`, `LootMode`, `G
 UPDATE `creature_template` SET `lootid` = 52151 WHERE `entry` = 52151;
  
 DELETE FROM `creature_loot_template` WHERE `entry`= 52151;
-INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `LootMode`, `MinCount`, `MaxCount`) VALUES
-(52151, 521510, 521510, 100, 1, 1, 1);
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(52151, 521510, 100, 521510, 1, 1, 1);
  
 DELETE FROM `reference_loot_template` WHERE `Entry` = 521510;
-INSERT INTO `reference_loot_template` (`Entry`, `Item`, `Chance`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`) VALUES
+INSERT INTO `reference_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
 (521510, 69605, 0, 1, 1, 1, 1), -- Amulet of the Watcher
 (521510, 69609, 0, 1, 1, 1, 1), -- Bloodlord's Protector
 (521510, 69608, 0, 1, 1, 1, 1), -- Deathcharged Wristguards
